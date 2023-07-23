a6c45f785c316dc2171503f0d4f295eae577247b
viemonkey <haducbaoviet@gmail.com>
2023-07-23 21:43:25 +0700

    done banner

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Banner from './components/pages/banner/banner'
+import NavBar from './components/pages/navbar/navbar'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+    </>
+  )
+}
+export default App
---
+++ src/components/pages/banner/banner.jsx
@@ -0,0 +1,11 @@
+import "./banner.css"
+
+export default function Banner() {
+    return (
+        <>
+            <div id="banner">
+                <img src="../../../../img/banner.jpg" alt="" />
+            </div>
+        </>
+    )
+}
